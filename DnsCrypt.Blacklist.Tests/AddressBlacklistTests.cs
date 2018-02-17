c617e3c20886fa970a9ea7e1e189975d2cc778d6
Christian Hermann <c.hermann@bitbeans.de>
2018-02-17 09:21:55 +0100

    Blacklist builder

---
+++ DnsCrypt.Blacklist.Tests/AddressBlacklistTests.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace DnsCrypt.Blacklist.Tests
+{
+	public class AddressBlacklistTests
+    {
+        [Fact]
+        public async void BuildTest()
+        {
+	        var whitelist = new List<string>
+	        {
+		        "a-msedge.net",
+		        "amazon.com",
+		        "appsflyer.com",
+		        "azurewebsites.net",
+		        "cdnetworks.com",
+		        "cloudapp.net",
+		        "edgekey.net",
+		        "elasticbeanstalk.com",
+		        "github.com",
+		        "github.io",
+		        "invalid",
+		        "j.mp",
+		        "l-msedge.net",
+		        "lan",
+		        "localdomain",
+		        "microsoft.com",
+		        "msedge.net",
+		        "nsatc.net",
+		        "ovh.net",
+		        "pusher.com",
+		        "pusherapp.com",
+		        "spotify.com",
+		        "tagcommander.com",
+		        "tracker.debian.org",
+		        "windows.net"
+			};
+
+			var blacklists = new List<string>
+	        {
+		        "file:Testfiles/domains-blacklist-local-additions.txt",
+		        "http://osint.bambenekconsulting.com/feeds/c2-dommasterlist.txt",
+		        "http://hosts-file.net/.%5Cad_servers.txt",
+		        "http://mirror1.malwaredomains.com/files/justdomains",
+		        "http://ransomwaretracker.abuse.ch/downloads/RW_DOMBL.txt",
+		        "http://www.malwaredomainlist.com/mdlcsv.php?inactive=off",
+		        "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt",
+		        "https://easylist-downloads.adblockplus.org/easylist_noelemhide.txt",
+		        "https://easylist-downloads.adblockplus.org/easylistchina.txt",
+		        "https://easylist-downloads.adblockplus.org/fanboy-social.txt",
+		        "https://pgl.yoyo.org/adservers/serverlist.php",
+		        "https://raw.githubusercontent.com/Dawsey21/Lists/master/adblock-list.txt",
+		        "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjxlist.txt",
+		        "https://raw.githubusercontent.com/liamja/Prebake/master/obtrusive.txt",
+		        "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt",
+		        "https://s3.amazonaws.com/lists.disconnect.me/simple_malware.txt",
+		        "https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt",
+		        "https://raw.githubusercontent.com/quidsup/notrack/master/trackers.txt",
+		        "http://sysctl.org/cameleon/hosts",
+		        "https://raw.githubusercontent.com/azet12/KADhosts/master/KADhosts.txt",
+		        "https://raw.githubusercontent.com/marktron/fakenews/master/fakenews",
+		        "http://mirror1.malwaredomains.com/files/dynamic_dns.txt",
+		        "https://raw.githubusercontent.com/Clefspeare13/pornhosts/master/0.0.0.0/hosts",
+		        "https://raw.githubusercontent.com/Sinfonietta/hostfiles/master/pornography-hosts",
+		        "http://securemecca.com/Downloads/hosts.txt",
+		        "https://raw.githubusercontent.com/Sinfonietta/hostfiles/master/gambling-hosts",
+		        "https://raw.githubusercontent.com/Sinfonietta/hostfiles/master/social-hosts"
+			};
+
+			var result = await AddressBlacklist.Build(blacklists, whitelist);
+			
+			Assert.NotNull(result);
+			Assert.Contains("eth0.me", result);
+        }
+    }
+}
---
+++ DnsCrypt.Blacklist/AddressBlacklist.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DnsCrypt.Blacklist
+{
+    public static class AddressBlacklist
+    {
+	    public static async Task<SortedSet<string>> Build(List<string> blacklistsSource, List<string> whitelistSource = null)
+	    {
+		    var blacklist = new SortedSet<string>();
+			var whitelist = new SortedSet<string>(whitelistSource);
+			foreach (var blacklistSourceEntry in blacklistsSource)
+		    {
+			    if (blacklistSourceEntry.StartsWith("file:"))
+			    {
+					if (!File.Exists(blacklistSourceEntry.Split(':')[1])) continue;
+					var rawListString = await ReadAllLinesAsync(blacklistSourceEntry.Split(':')[1]);
+					var parsed = ParseBlacklist(rawListString, true);
+				    foreach (var p in parsed)
+				    {
+					    blacklist.Add(p);
+				    }
+				}
+			    else
+			    {
+				    var rawListString = await FetchRemoteListAsync(blacklistSourceEntry);
+					if (rawListString == null) continue;			    
+					var parsed = ParseBlacklist(rawListString, false);
+				    foreach (var p in parsed)
+				    {
+					    blacklist.Add(p);
+				    }
+				}
+		    }
+		    blacklist.ExceptWith(whitelist);
+			return blacklist;
+	    }
+
+	    private static async Task<string> FetchRemoteListAsync(string requestUri)
+	    {
+		    try
+		    {
+			    using (var client = new HttpClient())
+			    {
+				    var getDataTask = client.GetStringAsync(requestUri);
+				    return await getDataTask.ConfigureAwait(false);
+			    }
+		    }
+		    catch (Exception)
+		    {
+
+		    }
+		    return null;
+	    }
+
+	    public static IEnumerable<string> ParseBlacklist(string blacklist, bool trusted = false)
+	    {
+		    var lines = blacklist.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+		    return ParseBlacklist(lines, trusted);
+	    }
+
+		public static IEnumerable<string> ParseBlacklist(IEnumerable<string> lines, bool trusted = false)
+	    {
+		    var names = new List<string>();
+		    var rxComment = new Regex(@"^(#|$)");
+		    var rxU = new Regex(@"^@*\|\|([a-z0-9.-]+[.][a-z]{2,})\^?(\$(popup|third-party))?$");
+		    var rxL = new Regex(@"^([a-z0-9.-]+[.][a-z]{2,})$");
+		    var rxH = new Regex(@"^[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}\s+([a-z0-9.-]+[.][a-z]{2,})$");
+		    var rxMdl = new Regex(@"^""[^""]+"",""([a-z0-9.-]+[.][a-z]{2,})"",");
+		    var rxB = new Regex(@"^([a-z0-9.-]+[.][a-z]{2,}),.+,[0-9: /-]+,");
+		    var rxTrusted = new Regex(@"^([*a-z0-9.-]+)$");
+
+		    foreach (var line in lines)
+		    {
+			    var tmp = line.ToLower().Trim();
+			    var regexList = new List<Regex>();
+			    if (trusted)
+			    {
+				    regexList.Add(rxTrusted);
+			    }
+			    else
+			    {
+				    regexList.Add(rxU);
+				    regexList.Add(rxL);
+				    regexList.Add(rxH);
+				    regexList.Add(rxMdl);
+				    regexList.Add(rxB);
+			    }
+
+			    var isComment = rxComment.Match(tmp);
+			    if (isComment.Success)
+			    {
+				    continue;
+			    }
+
+			    foreach (var regex in regexList)
+			    {
+				    var isMatching = regex.Match(tmp);
+				    if (!isMatching.Success)
+				    {
+					    continue;
+				    }
+				    if (!names.Contains(isMatching.Groups[1].Value))
+				    {
+					    names.Add(isMatching.Groups[1].Value);
+				    }
+			    }
+		    }
+		    return names;
+	    }
+
+
+	    private const int DefaultBufferSize = 4096;
+
+	    private const FileOptions DefaultOptions = FileOptions.Asynchronous | FileOptions.SequentialScan;
+
+	    private static Task<string[]> ReadAllLinesAsync(string path)
+	    {
+		    return ReadAllLinesAsync(path, Encoding.UTF8);
+	    }
+
+	    private static async Task<string[]> ReadAllLinesAsync(string path, Encoding encoding)
+	    {
+		    var lines = new List<string>();
+		    using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, DefaultBufferSize, DefaultOptions))
+		    using (var reader = new StreamReader(stream, encoding))
+		    {
+			    string line;
+			    while ((line = await reader.ReadLineAsync()) != null)
+			    {
+				    lines.Add(line);
+			    }
+		    }
+		    return lines.ToArray();
+	    }
+	}
+}
+
